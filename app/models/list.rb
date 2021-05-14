3a2ab445b515a0791533876c9df8b438642b015d
thibaultfltz <thibault.feltz@gmail.com>
2021-05-14 16:59:38 +0200

    Done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+  has_one_attached :image
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+  has_one_attached :image
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
   validates_length_of :overview, minimum: 6
